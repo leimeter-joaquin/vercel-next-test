8daed792d3fa8b99303685e62fb05406d5bd29a8
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-03-27 19:00:01 -0300

    RW-2: remove nice dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             Docs <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Find in-depth information abouadast Next.js .
+            Find in-depth information abouadast Nexdadasdasdt.js .
           </p>
         </a>
 
