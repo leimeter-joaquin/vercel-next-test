85470ee6ab2cd6c1b2252d271d68a69cce8b8808
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-03-27 18:45:24 -0300

    RW-1: remove those nice dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,9 @@ export default function Home() {
           <h2 className={inter.className}>
             Docs <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Find in-depth information about</p>
+          <p className={inter.className}>
+            Find in-depth informadadadadadaddadaation about
+          </p>
         </a>
 
         <a
