99cec05d56dbfde5de619ec53356b1cefe49e33c
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-03-27 18:45:06 -0300

    RW-1: add nice dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,9 +55,7 @@ export default function Home() {
           <h2 className={inter.className}>
             Docs <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js .
-          </p>
+          <p className={inter.className}>Find in-depth information about</p>
         </a>
 
         <a
