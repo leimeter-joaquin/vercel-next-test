367677c1a03974f28c576f896a17fdb9f07ca23d
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-03-28 18:04:22 -0300

    first commit on feature 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             Docs <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Find in-depth informationn about Next.js features and API.
+            Find in-depth information about Next.js features and API. 2
           </p>
         </a>
 
