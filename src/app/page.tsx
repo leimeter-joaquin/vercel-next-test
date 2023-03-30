804f9c10a20116bb6a64f3fae92ac3f6db5353fc
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-03-30 13:10:17 -0300

    add div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div></div>
       <div className={styles.description}>
         <div>
           <a
